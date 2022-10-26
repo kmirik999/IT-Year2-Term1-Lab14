3a97cf9ece80504159a874b55993893501a73905
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2022-10-26 21:12:22 +0300

    implement decrypt function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
+int wrap(int number, int maxvalue)
+{
+    if(number < 0)
+    {
+        number += maxvalue * (-number / maxvalue + 1); // для від'ємнних чисел що обернеться навколо нуля і границь
+    }
+    return number % maxvalue;
+}
 
 char* encrypt(char* rawText, int key)
 {
     int i = 0;
-    const char upperAlph[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 27 -- длина строки +1 пустое значение
+    const char upperAlph[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // 27 -- длина строки, +1 пустое значение
     const char lowerAlph[27] = "abcdefghijklmnopqrstuvwxyz";
     while (rawText[i] != NULL)
     {
@@ -18,13 +25,13 @@ char* encrypt(char* rawText, int key)
     {
         if(isalpha(rawText[i])) // isalpha -- чи є символ в  алфавіті
         {
-            if(isupper(rawText[i])) // проверяет находится ли в верхнем регистре
+            if(isupper(rawText[i])) // перевіряє, чи є  у верхньому регістрі
             {
-                text[i] = (upperAlph[((rawText[i] - 'A') + key) % 26]);
+                text[i] = upperAlph[wrap(rawText[i] - 'A' + key, 26)];
             }
             else
             {
-                text[i] = (lowerAlph[((rawText[i] - 'a') + key) % 26]);
+                text[i] = lowerAlph[wrap(rawText[i] - 'a' + key, 26)];
             }
         }
         else
@@ -35,8 +42,15 @@ char* encrypt(char* rawText, int key)
     }
     return text;
 }
+char* decrypt(char* encryptedText, int key)
+{
+    return encrypt(encryptedText, - key);
+}
+
 
 int main()
 {
-    cout << encrypt("Roses are red, violets are blue", 1);
+    char *encrypted = encrypt("Roses are red, violets are blue", 2);
+    cout << encrypted << endl;
+    cout << decrypt(encrypted, 2);
 }
